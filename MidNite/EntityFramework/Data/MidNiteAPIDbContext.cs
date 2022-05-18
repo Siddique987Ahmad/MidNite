aebc701dd524234796a556ce0d0bb85b4d32ac21
Waqar Hussain <Waqar Hussain@DESKTOP-5ARD3TQ>
2022-05-18 11:26:13 -0700

    Add project files.

---
+++ MidNite/EntityFramework/Data/MidNiteAPIDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MidNite.EntityFramework.Data
+{
+    public class MidNiteAPIDbContext
+    {
+    }
+}
