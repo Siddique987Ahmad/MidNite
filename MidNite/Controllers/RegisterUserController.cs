1f831ea219b3950e6584bbd19d2d84c09bef17aa
Waqar Hussain <Waqar Hussain@DESKTOP-5ARD3TQ>
2022-05-22 10:06:52 -0700

    API for RegisterUser  and Add Migration

--- MidNite/Controllers/RegisterUserController.cs
+++ MidNite/Controllers/RegisterUserController.cs
@@ -1,9 +1,11 @@
-﻿
-
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using MidNite.CommonHelpers;
+using MidNite.Core.RegisterUsers;
+using MidNite.EntityFramework.Data;
 using MidNite.Service.RegisterUsers;
+using System;
+using System.Globalization;
 using System.IO;
 
 namespace MidNite.Controllers
@@ -14,28 +16,42 @@ namespace MidNite.Controllers
     {
         private readonly RegisterUsersService _registerUsersService;
         private readonly IWebHostEnvironment _hostEnvironment;
-        public RegisterUserController(RegisterUsersService RegisterUserService, IWebHostEnvironment webHostEnvironment)
+        private readonly MidNiteAPIDbContext _midNiteAPIDbContext;
+        public RegisterUserController(RegisterUsersService RegisterUserService, MidNiteAPIDbContext midNiteAPIDbContext ,IWebHostEnvironment webHostEnvironment)
         {
             _registerUsersService = RegisterUserService;
             _hostEnvironment = webHostEnvironment;
+            _midNiteAPIDbContext = midNiteAPIDbContext;
         }
 
         [HttpPost]
         public ActionResult RegisterUser()
         {
+            RegisterUser registerUser = new RegisterUser();
             var FirstName = Request.Form["FirstName"].ToString();
             var LastName = Request.Form["LastName"].ToString();
             var UserName = Request.Form["UserName"].ToString();
             var Gender = Request.Form["Gender"].ToString();
             var Sexuailty = Request.Form["Sexuailty"].ToString();
-            var DateOfBrith = Request.Form["DateOfBrith"].ToString();
+            string DateOfBrith = Request.Form["DateOfBrith"].ToString();
+            DateTime dateTime = DateTime.ParseExact(DateOfBrith, new string[] { "mmmm.dd.yyyy", "mm.dd.yyyy", "m.d.yyyy", "mm/dd/yyyy", "m/d/yyyy", "mmmm/dddd/yyyy", "mm-dd-yyyy", "m-d-yyyy", "mmmm-dddd-yyyy", },
+                                       CultureInfo.InvariantCulture);
             var Password = Request.Form["Password"].ToString();
             var ProfileImage = Request.Form.Files;
-            //_registerUsersService.CreateUserAsync();
-            /// cuhange
-
+            
             if (ProfileImage.Count > 0)
             {
+                string path = Directory.GetCurrentDirectory();
+                if (path.Equals(Directory.GetCurrentDirectory()))
+                {
+                    
+                    string path2 = Path.Combine(path, "wwwroot");
+                    if (!Directory.Exists(path2))
+                    {
+                        Directory.CreateDirectory(path2);
+                    }
+                }
+
                 if (!Directory.Exists(_hostEnvironment.WebRootPath + "\\uploads\\"))
                 {
                     Directory.CreateDirectory(_hostEnvironment.WebRootPath + "\\uploads\\");
@@ -49,12 +65,27 @@ namespace MidNite.Controllers
 
                 if (CheckImageExtension)
                 {
+                    string DbPath;
                     using (FileStream filestream = System.IO.File.Create(_hostEnvironment.WebRootPath + "\\uploads\\" + "\\" + UserName + "\\" + ProfileImage[0].FileName))
                     {
                         ProfileImage[0].CopyTo(filestream);
+                        DbPath = filestream.Name;
                         filestream.Flush();
                     }
-                    return Ok();
+                    const int workFactor = 10;
+                    var HashedPassword = BCrypt.Net.BCrypt.HashPassword(Password, workFactor);
+                    registerUser.DateOfBrith = dateTime;
+                    registerUser.FirstName = FirstName;
+                    registerUser.LastName = LastName;
+                    registerUser.UserName = UserName;
+                    registerUser.Gender = Gender;
+                    registerUser.Sexuailty = Sexuailty;
+                    registerUser.Sexuailty = Sexuailty;
+                    registerUser.ImageProfilePath = DbPath;
+                    registerUser.Password = HashedPassword;
+                    _midNiteAPIDbContext.RegisterUsers.Add(registerUser);
+                    _midNiteAPIDbContext.SaveChanges();
+                    return Ok(registerUser);
                 }
                 else
                 {
